213d4e4586fd41ec8f3dd273667782261266bdb0
Juliandos <95juliandos@gmail.com>
2025-01-20 11:54:00 -0600

    Plantilla para página de inicio

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,4 +10,4 @@ app.config['SECRET_KEY'] = '@#qwr$"'
 
 @app.route('/')
 def index():
-    return 'Página principal'
+    return render_template('index.html')
